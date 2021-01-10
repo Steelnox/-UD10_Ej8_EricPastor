4a56309919a259339dae379f262442cafa8bda0f
Steelnox <eric14pastor@gmail.com>
2021-01-10 17:05:27 +0100

    Clase principal (Persona creada)

---
+++ UD10_Ej8/UD10_Ej8/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD10_Ej8
+{
+    public class Persona
+    {
+        protected string nombre;
+        protected int edad;
+        protected char sexo;
+
+        public virtual bool Asistencia()
+        {
+            return true;
+        }
+    }
+}
---
+++ UD10_Ej8/UD10_Ej8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UD10_Ej8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
